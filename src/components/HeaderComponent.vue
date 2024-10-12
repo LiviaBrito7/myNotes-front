94c2898e003f13cdff5cc12e2a4128f65004e674
Lívia Brito <analiviavicenar@gmail.com>
2024-10-12 20:05:40 -0300

    criando headerComponent

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn flat round dense icon="menu" class="q-mr-sm" />
+        <q-avatar>
+          <img src="../images/logo.svg">
+        </q-avatar>
+        <q-toolbar-title>MyNotes</q-toolbar-title>
+
+        <q-btn flat round dense icon="whatshot" />
+      </q-toolbar>
+    </q-header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderComponent'
+}
+</script>
+<style scoped>
+.q-header {
+  margin: 0;
+  padding: 0;
+  border: none;
+}
+</style>
