822bad587fe56c55ba2a3db9c22fb72dbacf2b1e
Lívia Brito <analiviavicenar@gmail.com>
2024-10-16 23:16:53 -0300

    estilizando header

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,14 +1,12 @@
 <template>
   <div class="q-pa-md">
-    <q-layout view="lHh lpr lFf" container style="height: 85px" class="shadow-2 rounded-borders">
+    <q-layout view="lHh lpr lFf" container class="shadow-2 rounded-borders" style="height: 85px;">
       <q-header elevated>
-        <q-toolbar>
+        <q-toolbar class="toolbar-content">
           <q-btn flat round dense icon="menu" class="q-mr-sm" />
-          <q-space></q-space>
-          <q-toolbar-title>
+          <q-toolbar-title class="toolbar-title">
             <strong>MyNotes</strong>
           </q-toolbar-title>
-          <q-space></q-space>
           <q-input
             v-model="searchQuery"
             label="Buscar Nota pelo Título"
@@ -22,7 +20,7 @@
               <q-icon name="search" class="bg-primary btn-search" />
             </template>
           </q-input>
-          <q-btn flat round dense icon="logout" @click="handleLogout" />
+          <q-btn flat round dense icon="logout" @click="handleLogout" color="primary" class="q-ml-sm" />
         </q-toolbar>
       </q-header>
     </q-layout>
@@ -54,28 +52,47 @@ export default {
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap');
 
+.toolbar-content {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+}
+
+.toolbar-title {
+  font-family: 'DM Sans';
+  color: #1976D2;
+  margin: 0 16px;
+}
+
 .header-search {
   margin: 15px;
   background-color: #ffffff;
   border-radius: 10px;
 }
-.q-field__label--float{
-  border-radius: 10px;
+
+.q-field__label--float {
+  border-radius: 50px;
 }
+
 .q-btn {
   color: white;
 }
+
 .btn-search {
   border-radius: 50px;
   width: 40px;
   height: 40px;
   color: white;
 }
+
 .btn-search:hover {
-  background-color: rgb(8, 68, 172)
+  background-color: rgb(8, 68, 172);
 }
-.q-header{
-  background-color: #999;
+
+.q-header {
+  background-color: white;
   font-family: 'DM Sans';
+  color: #1976D2;
 }
 </style>
