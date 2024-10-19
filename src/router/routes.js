6df34f5495ba61f53855e1029c1d32fb6e785b27
Lívia Brito <analiviavicenar@gmail.com>
2024-10-19 12:55:41 -0300

    tornando home a rota principal

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,9 +3,9 @@ const routes = [
     path: '/',
     component: () => import('layouts/SimpleLayout.vue'),
     children: [
+      { path: '', component: () => import('pages/HomePage.vue') },
       { path: 'register', component: () => import('pages/RegisterPage.vue') },
-      { path: 'login', component: () => import('pages/LoginPage.vue') },
-      { path: 'home', component: () => import('pages/HomePage.vue') }
+      { path: 'login', component: () => import('pages/LoginPage.vue') }
     ]
   },
 
